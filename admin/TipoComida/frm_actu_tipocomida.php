95d73802316a2c7f50dcc1bb1418c6098aa13cf5
Asfgm 123 <sgiraldo776@misena.edu.co>
2020-09-10 16:39:35 -0500

    Se agrega una carpeta de css y un archivo de estilos y se vincula con todas las paginas de form

--- admin/TipoComida/frm_actu_tipocomida.php
+++ admin/TipoComida/frm_actu_tipocomida.php
@@ -14,6 +14,7 @@ if ($fila = $sel -> fetch_assoc()) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"><!--Importacion css bootstrap-->
+    <link rel="stylesheet" type="text/css" href="../css/style.css">
     <title></title>
 </head>
 <body>
