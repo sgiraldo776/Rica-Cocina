ddb1f58e2763de41079e545373e6ac0e823dc9e4
Asfgm 123 <sgiraldo776@misena.edu.co>
2020-09-21 09:43:56 -0500

    Solucion chorrera de errores

--- admin/Usuario/form_usuario.php
+++ admin/Usuario/form_usuario.php
@@ -11,7 +11,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous"><!--Importacion css bootstrap-->
     <link rel="stylesheet" type="text/css" href="../css/style.css">
-    <link rel="stylesheet" type="text/css" href="../css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="../css/style1.css">
     <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;700&display=swap" rel="stylesheet"> 
     <title>Registrarse</title>
 </head>
--- vistas/recetas.php
+++ vistas/recetas.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
         integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <!--Importacion css bootstrap-->
-    <link rel="stylesheet" type="text/css" href="../admin/css/estilo.css">
+    <link rel="stylesheet" type="text/css" href="../admin/css/style1.css">
     <title>Rica Cocina</title>
 </head>
 
