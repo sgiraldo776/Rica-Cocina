b7a94f39450745e35b552c8ca75e399a3a70e1a2
sam-oro <samiroroz@gmail.com>
2020-09-22 10:43:06 -0500

    Login usuario

---
+++ vistas/login/iniciar_sesion.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <!--Importacion css bootstrap-->
+    <link rel="stylesheet" type="text/css" href="../../admin/css/style.css">
+    <link rel="stylesheet" type="text/css" href="../../admin/css/style1.css">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main>
+
+        <div class="contenedor col-12 text-center">
+            <div class="row col-12">
+                <div class="col-12">
+                    <form action="">
+                        <label for=""> Nombre de Usuario</label>
+                        <input type="text" class="form-control" placeholder="Digita tu nombre de usuario aquí">
+                        <label for="">Contraseña</label>
+                        <input type="password" class="form-control" placeholder="Tu contraseña">
+                        <a href=""><span>¿Olvidaste tu contraseña?</span></a>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+
+    <!--JS de bootstrap-->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+
+    <!--validacion de capos vacios-->
+    <script type="text/javascript" src="js/ValidarUsuario.js"></script>
+    <!--validacion de capos vacios-->
+
+</body>
+
+</html>
