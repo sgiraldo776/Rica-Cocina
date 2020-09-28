2ebe326ed33806ef90ad2137a96fc71142354d29
sam-oro <samiroroz@gmail.com>
2020-09-28 16:38:50 -0500

    img add and more styles

---
+++ vistas/receta-individual/mostrar-receta.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&family=Quicksand:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="icon" type="image/png" href="../img/favicon.png">
+    <!--Importacion css bootstrap-->
+    <link rel="stylesheet" type="text/css" href="../../admin/css/styles1.css">
+    <link rel="stylesheet" type="text/css" href="../../admin/css/style.css">
+    <title>Rica Cocina</title>
+</head>
+
+<body>
+    <header class="site-header" id="nav">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="../../index.php">
+                    <img src="../../img/logo-rica-cociona3.png" class="logo" alt="Logotipo de Rica Cocina">
+                </a>
+                <nav class="navegacion">
+                    <a href="vistas/recetas.php">Recetas</a>
+                    <a href="admin/Usuario/form_usuario.php">Registrar</a>
+                    <a href="#">Inicia Sesión</a>
+                </nav>
+            </div>
+        </div>
+    </header>
+
+    <main>
+        <div class="contenedor ingredientes">
+            <div class="row">
+                <div class="col-4">
+                    <ul>
+                        <li><img src="../../img/num-per.png" alt=""></li>
+                        <li><img src="../../img/paises.png" alt=""></li>
+                        <li><img src="../../img/tipo-comida.png" alt=""></li>
+                        <li><img src="../../img/tipo-receta.png" alt=""></li>
+                        <li><img src="../../img/tipo-dieta.png" alt=""></li>
+                        <li><img src="../../img/ocasion.png" alt=""></li>
+                        <li><img src="../../img/tiempo.png" alt=""></li>
+                    </ul>
+                </div>
+                <div class="col-4">
+                    <h1>Ingredientes</h1>
+                    <p></p>
+                </div>
+                <div>
+                    <img src="" alt="">
+                </div>
+            </div>
+        </div>
+
+    </main>
+
+</body>
+
+</html>
+
+<!--  <img src="../../img/num-per.png" width="45px">  -->
