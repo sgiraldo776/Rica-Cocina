2a8e07350e1a7d023f2a154215bd22d6e5941574
Juangarzon11 <juangarzon07@gmail.com>
2020-10-08 09:43:25 -0500

    Eliminar Receta con Confirmacion

--- vistas/ingreso/eliminar_receta.php
+++ vistas/ingreso/eliminar_receta.php
@@ -5,16 +5,24 @@ $id =$_REQUEST['recetaid'];
 
 $del1 = $conn -> query("DELETE FROM tblrecetautensilio WHERE recetaid ='$id'");
 
+
 if($del1 === TRUE){
-    $del = $conn -> query("DELETE FROM tblreceta WHERE recetaid = '$id' ");
+    $del2= $conn -> query("DELETE FROM tblretroalimentacion WHERE recetaid='$id'");
+    if($del2 === TRUE){
+        $del = $conn -> query("DELETE FROM tblreceta WHERE recetaid = '$id' ");
     if ($del === TRUE) {
-        //echo "<script> 	location.href='../mis_recetas.php'; </script>";
+        echo "<script> 	location.href='../mis_recetas.php'; </script>";
     }else{
-        //echo "Error: " . $del . "<br>". $conn->error;
+        echo "Error: " . $del . "<br>". $conn->error;
         //echo "<script> alert('El registro no pudo ser eliminado'); 	location.href='../mis_recetas.php'; </script>";
     }
+    }else{
+        echo "Error: " . $del . "<br>". $conn->error;
+        //echo "<script> alert('El registro no pudo ser eliminado'); 	location.href='../mis_recetas.php'; </script>";
+    }
+    
 }else{
-    //echo "Error: " . $del1 . "<br>". $conn->error;
+    echo "Error: " . $del1 . "<br>". $conn->error;
     //echo "<script> alert('El registro no pudo ser eliminado'); 	location.href='../mis_recetas.php'; </script>";
 }
 
--- vistas/mis_recetas.php
+++ vistas/mis_recetas.php
@@ -58,11 +58,12 @@
                             </a>
                         </div>
                         
-                        <form action="ingreso/eliminar_receta.php?recetaid=<?php echo $row[0] ?>" name="add_form" method="POST">
-                        <input type="text" id="recetaid" name="recetaid" value="<?php echo $row[0] ?>" >
+                        <form action="" name="add_form" method="POST">
+                        <input type="text" id="recetaid" name="recetaid" value="<?php echo $row[0] ?>" hidden>
                             <a href="ingreso/frm-actu-receta.php?recetaid=<?php echo $row[0] ?>">EDITAR</a>
                             <!--<button type="button" onclick="validareliminar()"><a id="eliminar" href="">ELIMINAR</a></button>-->
-                            <button type="button" onclick="validareliminar()" class="boton boton-amarillo" id="eliminar">eliminar</button>
+                            <!--<button type="button" class="boton boton-amarillo" id="eliminar">eliminar</button>-->
+                            <a href="#" onclick="preguntar(<?php echo $row[0]?>)">ELIMINAR</a>
                         </form>
                     </div>
                     
@@ -101,10 +102,35 @@
         </div>
     </footer>
 
+    <script type="text/javascript">
+        function preguntar(id){
+           Swal
+            .fire({
+                title: "¿Eliminar Receta?",
+                text: "¿Estas seguro de Eliminar la Receta?",
+                icon: 'error',            
+                showCancelButton: true,
+                confirmButtonText: "Sí, eliminar",
+                cancelButtonText: "Cancelar",
+            })
+            .then(resultado => {
+                if (resultado.value) {
+                    // Hicieron click en "Sí"
+                    //console.log("*se elimina la venta*");
+                    window.location.href="ingreso/eliminar_receta.php?recetaid="+id
+                } else {
+                    // Dijeron que no
+                    console.log("*NO se elimina la venta*");
+                }
+            });
+
+        }
+    </script>
+
     
 
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10"></script>
-    <script src="../admin/js/alerts.js"></script>
+    <!--<script src="../admin/js/alerts.js"></script>-->
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"crossorigin="anonymous"></script>
