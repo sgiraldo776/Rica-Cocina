77f7b9dbbf13dcbfe98f85699d8493934535cc09
anderson.castano <anderson.castano@pragma.com.co>
2021-02-27 14:57:20 -0500

    Fix error

--- admin/TipoComida/frm_actu_tipocomida.php
+++ admin/TipoComida/frm_actu_tipocomida.php
@@ -18,7 +18,7 @@ if ($fila = $sel -> fetch_assoc()) {
 
  ?>
  <!DOCTYPE html>
- <html>
+ <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
